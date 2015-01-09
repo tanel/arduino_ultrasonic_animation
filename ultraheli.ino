785fbeb57dfae9bd40dba8c4ddc0579530b6e7ee
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-01-09 08:40:26 -0500

    Initial commit

---
+++ ultraheli.ino
@@ -0,0 +1,57 @@
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
+ on 10 Nov 2012.
+ */
+
+
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+int speakerOut = 9;
+
+long duration, distance; // Duration used to calculate distance
+
+void setup() {
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(speakerOut, OUTPUT);
+}
+
+void beep(unsigned char delayms){
+  analogWrite(9, 20);      // Almost any value can be used except 0 and 255
+                           // experiment to get the best tone
+  delay(delayms);          // wait for a delayms ms
+  analogWrite(9, 0);       // 0 turns it off
+  delay(delayms);          // wait for a delayms ms   
+}
+
+void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ 
+ int value = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ Serial.println(distance);
+ 
+ delay(50);
+
+ duration = value;
+}
